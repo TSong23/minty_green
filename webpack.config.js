00fce3b347e2229151023b2651c06860b8aa66c7
Taehoon <songyi3279@gmail.com>
2019-09-08 20:38:25 -0700

    created session and user api controllers. made static pages. routes default json format

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/mintgreen.jsx',
+  entry: './frontend/minty_green.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
