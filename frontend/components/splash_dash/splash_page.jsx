7be0a587cb66e239a9e2ce73e60d5d6babceb228
Taehoon <songyi3279@gmail.com>
2019-09-11 19:50:21 -0700

    more css. done with splash page. move on to dash board

--- frontend/components/splash_dash/splash_page.jsx
+++ frontend/components/splash_dash/splash_page.jsx
@@ -24,19 +24,20 @@ class SplashPage extends React.Component{
         <div className="splash_page_body">
           <div className="splash_page_body_text">
             <h3>Invest Commission-Free</h3> 
-            <h4>Random Text</h4>
+            <br/>
+            <h4>Invest in stocks, ETFs, options, and cryptocurrencies,
+               all commission-free, right from your phone or desktop.</h4>
           </div>
-          <img src={window.images.splash_background} className="robin_phones_img" />
+          <img src={window.images.splash_background}
+           className="splash_page_image" />
         </div>
 
-        <footer>
-          <h3>
-            <ul>
-              <li>Portfolio Site</li>
-              <li>Linked In</li>
-              <li>GitHub</li>
-            </ul>
-          </h3>
+        <footer className="footer">          
+          <h3>Portfolio Site</h3>
+          <br/>
+          <h3>Linked In</h3>
+          <br/>
+          <h3>GitHub</h3>               
         </footer>
 
       </div>
