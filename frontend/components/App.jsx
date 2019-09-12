b735587d814afec35e4f328892590de680a9ff0a
Taehoon <songyi3279@gmail.com>
2019-09-12 12:22:06 -0700

    fixed redirect

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import GreetingContainer from "./greeting/greeting_container";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 
