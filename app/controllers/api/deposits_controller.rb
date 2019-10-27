1c5fd0ec7fe031906cf877a6595bb250d16b86ef
Taehoon <songyi3279@gmail.com>
2019-10-27 13:23:59 -0700

    begin working on controllers and routes

--- app/controllers/api/deposits_controller.rb
+++ app/controllers/api/deposits_controller.rb
@@ -1,2 +1,17 @@
 class Api::DepositsController < ApplicationController
+  def create
+    @deposit = Deposit.new(deposit_params)
+    @deposit.user_id = current_user.id
+    if @deposit.save
+      render json: ["Deposit Successful"], status: 200
+    else
+      render json: @deposit.full_messages, status: 422
+    end
+  end
+
+  private
+  def deposit_params
+    params.require(:deposit).permit(:amount)
+  end
+
 end
--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -1,2 +1,17 @@
 class Api::TransactionsController < ApplicationController
+  def create 
+    @transaction = Transaction.new(transaction_params)
+    @transaction.user_id = current_user.id
+    if @transaction.save
+      render json: ['Purchase Success!'], status: 200
+    else
+      render json: @transaction.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:stock_id, :num_shares, :price, :order_type)
+  end
 end
