60642de12c8ce2b62c2932f1da34329bc401a98c
Taehoon <songyi3279@gmail.com>
2019-10-28 11:15:43 -0700

    take off watchlist from global state to minimize rerenders on home page. watchlist component will import ajax functions directly and should not be part of redux store

--- frontend/actions/watchlist_actions.js
+++ frontend/actions/watchlist_actions.js
@@ -1,5 +1,11 @@
 import * as WatchlistApiUtil from '../util/watchlist_api_util';
 
+// to minimize rerenders on frontend, import api util directly to watchlist 
+// component and save to local state of the component rather than in the global 
+// state
+
+
+
 export const RECEIVE_WATCHLISTS = "RECEIVE_WATCHLISTS";
 export const RECEIVE_WATCHLIST = "RECEIVE_WATCHLIST";
 export const REMOVE_WATCHLIST = "REMOVE_WATCHLIST";
@@ -10,11 +16,11 @@ export const fetchAllWatchlist = () => dispatch => (
   )
 );
 
-export const fetchWatchlist = (id) => dispatch => (
-  WatchlistApiUtil.fetchWatchlist(id).then(
-    watchlist => dispatch(receiveWatchlist(watchlist))
-  )
-);
+// export const fetchWatchlist = (id) => dispatch => (
+//   WatchlistApiUtil.fetchWatchlist(id).then(
+//     watchlist => dispatch(receiveWatchlist(watchlist))
+//   )
+// );
 
 export const createWatchlist = (watchlist) => dispatch => (
   WatchlistApiUtil.createWatchlist(watchlist).then(
@@ -22,11 +28,11 @@ export const createWatchlist = (watchlist) => dispatch => (
   )
 );
 
-export const updateWatchlist = (watchlist) => dispatch => (
-  WatchlistApiUtil.updateWatchlist(watchlist).then(
-    watchlist => dispatch(receiveWatchlist(watchlist))
-  )
-);
+// export const updateWatchlist = (watchlist) => dispatch => (
+//   WatchlistApiUtil.updateWatchlist(watchlist).then(
+//     watchlist => dispatch(receiveWatchlist(watchlist))
+//   )
+// );
 
 export const deleteWatchlist = (watchlistId) => dispatch => (
   WatchlistApiUtil.deleteWatchlist(watchlistId).then(
@@ -40,10 +46,10 @@ const receiveAllWatchlist = watchlists => ({
   watchlists
 });
 
-const receiveWatchlist = watchlist => ({
-  type: RECEIVE_WATCHLIST,
-  watchlist
-});
+// const receiveWatchlist = watchlist => ({
+//   type: RECEIVE_WATCHLIST,
+//   watchlist
+// });
 
 const removeWatchlist = watchlistId => ({
   type: REMOVE_WATCHLIST,
